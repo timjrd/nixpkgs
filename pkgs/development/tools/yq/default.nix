5707c39ae22cd1c8d7e3cfb41fa3fdc40bcc5c16
Frederik Rietdijk <fridh@fridh.nl>
2018-06-12 18:47:08 +0200

    python: yq: 2.5.0 -> 2.6.0

--- pkgs/development/tools/yq/default.nix
+++ pkgs/development/tools/yq/default.nix
@@ -2,7 +2,7 @@
 
 buildPythonApplication rec {
   pname = "yq";
-  version = "2.5.0";
+  version = "2.6.0";
 
   propagatedBuildInputs = [ pyyaml xmltodict jq ];
 
@@ -11,7 +11,7 @@ buildPythonApplication rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "e9fd2aa32defcd051058e3b6f665873282aa4eed19e11b1db94fe70847535d4c";
+    sha256 = "c64f763e8409ed55eb055793c26fc347b5a6666b303d49e9d2f8d7cea979df73";
   };
 
   meta = with lib; {
